f465b5022d7b3d068b0c58fec5523c5f2191602a
Efe <efesezen1@outlook.com>
2024-11-24 12:44:37 +0300

    First commit, typescript practice done well.

---
+++ src/main.ts
@@ -0,0 +1,103 @@
+interface Todo {
+   readonly id: number
+   checked: Boolean
+   text: string
+}
+
+let todos: Todo[] = []
+
+const input: HTMLInputElement | null = document.getElementById(
+   'todo-input'
+) as HTMLInputElement | null
+
+const list_container: HTMLElement | null = document.getElementById('todo-list')
+
+const todosHTML = (): string[] =>
+   todos.map((todo: Todo) => {
+      return `
+   <li
+   id=${todo.id}
+                  class="todo-item rounded bg-blue-200 p-5 flex justify-between items-center my-1"
+                  
+               >
+                  <div>
+                     <input
+                      ${todo.checked ? 'checked' : ''}
+                        type="checkbox"
+                        name="todo-check"
+                        id="todo-check"
+                        class="mr-2"
+                        value="todo-check"
+                     />
+                     <span class="capitalize ${
+                        todo.checked ? 'line-through' : ''
+                     }" > ${todo.text} </span>
+                  </div>
+                  <button
+                     class="hover:scale-110 active:scale-95 transition-all duration-300 ease-out bg-blue-500 w-5 h-5 flex justify-center items-center rounded-full p-4 text-white"
+                  >
+                     <i class="fa-solid fa-xmark"></i>
+                  </button>
+               </li>
+  `
+   })
+
+input?.addEventListener('keydown', (e: KeyboardEvent) => {
+   console.log(e)
+   if (e.key === 'Enter') {
+      const todoHTML = e.target as HTMLInputElement
+      const todo: string = todoHTML.value
+      const todoId: string = String(Math.random() * 100)
+         .split('.')
+         ?.at(1)
+      const todoItem: Todo = { checked: false, text: todo, id: todoId }
+      if (todo === '') return
+      todos.push(todoItem)
+      renderHTML()
+      todoHTML.value = ''
+   }
+})
+
+const renderHTML = (): void => {
+   ;(list_container as HTMLElement).innerHTML = todosHTML().join('')
+}
+
+list_container?.addEventListener('click', (e: MouseEvent) => {
+   const target = e.target as HTMLElement
+   const todoItem = target.closest('.todo-item')
+
+   if (todoItem) {
+      const todoId: string | null = todoItem.getAttribute('id')
+      console.log('Clicked todo item with ID:', todoId)
+
+      // If checkbox was clicked
+      if (target.matches('input[type="checkbox"]')) {
+         console.log('Checkbox clicked')
+         const checkbox = target as HTMLInputElement
+         const isChecked = checkbox.checked
+         console.log('Checkbox is checked:', isChecked)
+
+         if (todoId) {
+            const todo = todos.find((todo) => todo.id.toString() === todoId)
+            if (todo) {
+               console.log(isChecked)
+               todo.checked = isChecked
+               console.log(todo)
+
+               renderHTML()
+            }
+         }
+      }
+
+      // If delete button was clicked
+      if (target.matches('button') || target.matches('i.fa-xmark')) {
+         console.log('Delete button clicked')
+         todos = todos.filter((todo) => {
+            if (typeof todo.id !== 'string' || typeof todoId !== 'string')
+               return
+            return todo.id !== todoId
+         })
+         ;(list_container as HTMLElement).innerHTML = todosHTML().join('')
+      }
+   }
+})
